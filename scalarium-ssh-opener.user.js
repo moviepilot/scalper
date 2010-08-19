601dfb7762a8fcd375a451b86e1c11fb8bf9a025
Daniel Bornkessel <github@bornkessel.com>
2010-08-19 15:11:41 +0200

    ...

--- scalarium-ssh-opener.user.js
+++ scalarium-ssh-opener.user.js
@@ -4,25 +4,5 @@
 // @description    adds an ssh://ubuntu@<server> link next to the java console link
 // @include        https://*.scalarium.com/*
 // ==/UserScript==
-
-
-// Add jQuery
-    var GM_JQ = document.createElement('script');
-    GM_JQ.src = 'http://jquery.com/src/jquery-latest.js';
-    GM_JQ.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(GM_JQ);
-
-// Check if jQuery's loaded
-    function GM_wait() {
-      if(typeof unsafeWindow.jQuery == 'undefined') { window.setTimeout(GM_wait,100); }
-      else { jQuery = unsafeWindow.jQuery; letsJQuery(); }
-    }
-    GM_wait();
-
-// All your GM code must be inside this function
-    function letsJQuery() {
-      jQuery.noConflict();
-
-      jQuery('[data-ip]').append("<a href='#' onclick='location.href=\"ssh://ubuntu@\" + $(this).parent().attr(\"data-ip\")'>open externally</a>")
-    }
+      $('[data-ip]').append("<a href='#' onclick='location.href=\"ssh://ubuntu@\" + $(this).parent().attr(\"data-ip\")'>open externally</a>")
 
